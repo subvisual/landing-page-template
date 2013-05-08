9c09c8bf61ce2d0ab170d7ab351317ecde4b023a
Gabriel Poça <gabrielpoca@gmail.com>
2013-05-08 10:16:30 +0100

    New rails app with -T for no test unit. Added pry, better_errors, foundation and compass.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LandingPageTemplate::Application.config.secret_token = '8837d99ec1a2053a93791539c7eaa50b7cd111c5fa9ba9f14a57caa6ec789aa0fb1a555dd583e7c2f00050fd5308f92fccf0f49c6569bdb97e12fdf41f3e0e8a'
