a2507b0259c2edb64efaa48eca7cff156a382f4c
Gabriel Poça <gabrielpoca@gmail.com>
2013-05-08 15:12:13 +0100

    'Added mixpanel email signup javascript function.'

--- config/initializers/variables.rb
+++ config/initializers/variables.rb
@@ -1,6 +1,10 @@
 # define custom variables for mixpanel and mailchimp
+
+
+# mixpanel variables
+MIXPANEL_SIGNUP_EVENT = 'signup'
 if Rails.env == 'development' || Rails.env == 'test'
-  #MIXPANEL_TOKEN = '#development'
+  MIXPANEL_TOKEN = '#development'
 else 
-  #MIXPANEL_TOKEN = '#production'
+  MIXPANEL_TOKEN = '#production'
 end
