63729253ca31cffbdda2d6020c57bab112bf2924
Gabriel Poça <gabrielpoca@gmail.com>
2013-05-22 10:27:15 +0100

    Added token to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130508165557) do
+ActiveRecord::Schema.define(:version => 20130522091725) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "token"
   end
 
 end
