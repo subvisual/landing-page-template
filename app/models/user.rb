7cf45ab88500588ec3dbdb7cb71c4e9f891d77c3
Gabriel Poça <gabrielpoca@gmail.com>
2013-05-22 10:27:04 +0100

    Added token to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :token
+
+  before_create :generate_token
+
+  def generate_token
+    self.token = Digest::MD5.hexdigest self.email
+  end
 end
