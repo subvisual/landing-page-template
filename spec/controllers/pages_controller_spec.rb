f04b54eb11e99cee41a9b73fd4b6fa7f7700276f
Gabriel Poça <gabrielpoca@gmail.com>
2013-06-05 11:36:00 +0100

    updated rspec tests with toughtbot guides.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,9 @@ describe PagesController do
 
   it 'updates the views counter when someone follows the referral' do
     user = FactoryGirl.create :user
+
     get :referral, token: user.token
+
     User.find(user).referral_views.should eql 1
   end
 
